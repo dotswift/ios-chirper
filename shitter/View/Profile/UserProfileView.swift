1bc791adbef5dc708929374cb3031cb5d094aa21
derek quinn <quinnderek@yahoo.com>
2021-01-08 20:58:19 -0500

    Section 3 video 17

--- shitter/View/Profile/UserProfileView.swift
+++ shitter/View/Profile/UserProfileView.swift
@@ -1,11 +1,20 @@
 import SwiftUI
 
 struct UserProfileView: View {
+    @State var selectedFilter: TweetFilterOptions = .tweets
+    
     var body: some View {
         ScrollView{
             VStack{
                 ProfileHeaderView()
                     .padding()
+                FilterButtonView(selectedOption: $selectedFilter)
+                    .padding()
+                
+                ForEach(0..<9) { tweet in
+                    TweetCell()
+                        .padding()
+                }
             }
             .navigationTitle("Shilling")
         }
