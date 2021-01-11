27cfcc7ab07844b6c5129dc3ccef2ce89288c46c
derek quinn <quinnderek@yahoo.com>
2021-01-10 21:17:52 -0500

    Section 5 video 24

--- shitter/Utilities/Views/ImagePicker.swift
+++ shitter/Utilities/Views/ImagePicker.swift
@@ -1,13 +1,16 @@
 import SwiftUI
 
 struct ImagePicker: UIViewControllerRepresentable { // wrapper to show ViewController in Swift UI
+    @Binding var image: UIImage? // this is a UIImage (not a SwiftUI image) because below imagePickerController() returns a UIImage
+    @Environment(\.presentationMode) var mode // adds ability to call .dismiss (line 34)
     
     func makeCoordinator() -> Coordinator { // The Coordinator helps the View Controller communicate with the SwiftUI functionality (its the link)
         Coordinator(self) // briging the gap between UIKit and SwiftUI
     }
     
     func makeUIViewController(context: Context) -> some UIViewController { // you  can put any view controller class in here (can be custom)
-        let picker = UIImagePickerController() // for example, this is not natively available in SwiftUI, only un UIKit
+        let picker = UIImagePickerController() // for example, this is not natively available in SwiftUI, only un UIKitasdfas
+        picker.delegate = context.coordinator //  in protocol oriented programming, you have to set the delegate property to make imagePickerController() get called
         return picker
     }
     
@@ -18,7 +21,7 @@ struct ImagePicker: UIViewControllerRepresentable { // wrapper to show ViewContr
 
 extension ImagePicker {
     class Coordinator: NSObject, UINavigationControllerDelegate, UIImagePickerControllerDelegate{
-        let parent: ImagePicker
+        let parent: ImagePicker // parent IS the Image Picker Struct (above)
         
         init(_ parent: ImagePicker){
             self.parent = parent
@@ -26,6 +29,9 @@ extension ImagePicker {
         
         func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
             // this code is executed when we select an image from the image picker
+            guard let image = info[.originalImage] as? UIImage else {return}
+            parent.image = image // set the image
+            parent.mode.wrappedValue.dismiss() // dismiss the view after setting the image
         }
     }
 }
--- shitter/View/Authentication/RegistrationView.swift
+++ shitter/View/Authentication/RegistrationView.swift
@@ -6,90 +6,108 @@ struct RegistrationView: View {
     @State var fullName = ""
     @State var userName = ""
     @State var showImagePicker = false
+    @State var selectedUIImage: UIImage? // UIKit
+    @State var image: Image? // Swift UI Image
+    
     @Environment(\.presentationMode) var mode: Binding<PresentationMode>
     // variable that keeps track of the presentation mode that app is currently utilzing
     
-    var body: some View {
-
-            ZStack {
-                VStack {
-                    Button(action: {
-                        showImagePicker.toggle()
-                     
-                        
-                    }, label: {
-                        Image("plus_photo")
-                            .resizable()
-                            .renderingMode(.template)
-                            .scaledToFill()
-                            .frame(width: 140, height: 140)
-                            .padding(.top, 50)
-                            .padding(.bottom, 16)
-                            .foregroundColor(.yellow)
-                    }).sheet(isPresented: $showImagePicker, content: {
-                        // Using UIKit for image picker functionality
-                        ImagePicker()
-                        
-                    })
-                    
-                    VStack(spacing: 20){
-                        CustomTextField(text: $email, placeholder: Text("E-Mail"), imageName: "envelope")
-                            .padding() // this padding spreads text field dimensions
-                            .background(Color(.init(white: 1, alpha: 0.15)))
-                            .cornerRadius(30)
-                            .foregroundColor(.white)
-                        
-                        CustomTextField(text: $fullName, placeholder: Text("Full Name"), imageName: "figure.walk")
-                            .padding() // this padding spreads text field dimensions
-                            .background(Color(.init(white: 1, alpha: 0.15)))
-                            .cornerRadius(30)
-                            .foregroundColor(.white)
-                        
-                        CustomTextField(text: $userName, placeholder: Text("User Name"), imageName: "figure.wave")
-                            .padding() // this padding spreads text field dimensions
-                            .background(Color(.init(white: 1, alpha: 0.15)))
-                            .cornerRadius(30)
-                            .foregroundColor(.white)
-                        
-                        CustomSecureField(text: $password, placeholder: Text("password"), imageName: "tortoise")
-                            .padding() // this padding spreads text field dimensions
-                            .background(Color(.init(white: 1, alpha: 0.15)))
-                            .cornerRadius(30)
-                            .foregroundColor(.white)
-                    }
-                    .padding(.horizontal, 32)
-                    
-                    Button(action: /*@START_MENU_TOKEN@*/{}/*@END_MENU_TOKEN@*/, label: {
-                        Text("Register your Restaurant")
-                            .font(.headline)
-                            .foregroundColor(.yellow)
-                            .frame(width: 360, height: 50)
-                            .background(Color.white)
-                            .clipShape(Capsule())
-                            .padding()
-                    })
-                    
-                    Spacer()
-                    
-                    Button(action:  {mode.wrappedValue.dismiss()}, label: {
-                        HStack {
-                            Text("Already have an account?")
-                                
-                                .font(.system(size: 14, weight: .semibold))
-                            
-                            Text("Sign in here.")
-                                .font(.system(size: 14, weight: .semibold))
-                        }
-                        .foregroundColor(.white)
-                        .padding(.bottom, 40)
-                    })
-                }
-                .background(Color(.brown))
-            }
-            .background(Color(#colorLiteral(red: 0.9686274529, green: 0.78039217, blue: 0.3450980484, alpha: 1)))
-            .ignoresSafeArea()
-        }
+    func loadImage(){
+        guard let selectedImage = selectedUIImage else { return }
+        image = Image(uiImage: selectedImage) //  converts UIKit image to SwiftUI Image
     }
+    
+    var body: some View {
+        ZStack {
+            VStack {
+                Button(action: {
+                    showImagePicker.toggle()
+                    
+                }, label: {
+                    ZStack { // if an image has been selected show it, if not show plus_photo
+                        if let image = image { // CONTROL FLOW // re-renders the state  variable $image
+                            image // runs if image has a value
+                                .resizable()
+                                .scaledToFill()
+                                .frame(width: 140, height: 140)
+                                .clipped()
+                                .cornerRadius(70)
+                                .padding(.top, 50)
+                                .padding(.bottom, 16)
+                            
+                        } else {
+                            Image("plus_photo")
+                                .resizable()
+                                .renderingMode(.template)
+                                .scaledToFill()
+                                .frame(width: 140, height: 140)
+                                .padding(.top, 50)
+                                .padding(.bottom, 16)
+                                .foregroundColor(.yellow)
+                        }
+                    }
+                }).sheet(isPresented: $showImagePicker, onDismiss: loadImage, content: {
+                    ImagePicker(image: $selectedUIImage) // when ImagePicker is dimissed, loadImage() is called
+                })
+                
+                VStack(spacing: 20){
+                    CustomTextField(text: $email, placeholder: Text("E-Mail"), imageName: "envelope")
+                        .padding() // this padding spreads text field dimensions
+                        .background(Color(.init(white: 1, alpha: 0.15)))
+                        .cornerRadius(30)
+                        .foregroundColor(.white)
+                    
+                    CustomTextField(text: $fullName, placeholder: Text("Full Name"), imageName: "figure.walk")
+                        .padding() // this padding spreads text field dimensions
+                        .background(Color(.init(white: 1, alpha: 0.15)))
+                        .cornerRadius(30)
+                        .foregroundColor(.white)
+                    
+                    CustomTextField(text: $userName, placeholder: Text("User Name"), imageName: "figure.wave")
+                        .padding() // this padding spreads text field dimensions
+                        .background(Color(.init(white: 1, alpha: 0.15)))
+                        .cornerRadius(30)
+                        .foregroundColor(.white)
+                    
+                    CustomSecureField(text: $password, placeholder: Text("password"), imageName: "tortoise")
+                        .padding() // this padding spreads text field dimensions
+                        .background(Color(.init(white: 1, alpha: 0.15)))
+                        .cornerRadius(30)
+                        .foregroundColor(.white)
+                }
+                .padding(.horizontal, 32)
+                
+                Button(action: /*@START_MENU_TOKEN@*/{}/*@END_MENU_TOKEN@*/, label: {
+                    Text("Register your Restaurant")
+                        .font(.headline)
+                        .foregroundColor(.yellow)
+                        .frame(width: 360, height: 50)
+                        .background(Color.white)
+                        .clipShape(Capsule())
+                        .padding()
+                })
+                
+                Spacer()
+                
+                Button(action:  {mode.wrappedValue.dismiss()}, label: {
+                    HStack {
+                        Text("Already have an account?")
+                            
+                            .font(.system(size: 14, weight: .semibold))
+                        
+                        Text("Sign in here.")
+                            .font(.system(size: 14, weight: .semibold))
+                    }
+                    .foregroundColor(.white)
+                    .padding(.bottom, 40)
+                })
+            }
+            .background(Color(.brown))
+        }
+        .background(Color(#colorLiteral(red: 0.9686274529, green: 0.78039217, blue: 0.3450980484, alpha: 1)))
+        .ignoresSafeArea()
+    }
+}
 
 struct RegistrationView_Previews: PreviewProvider {
     static var previews: some View {
