ff895717e387c8d774f2a63e9133323cd665dc2c
derek quinn <quinnderek@yahoo.com>
2021-01-30 17:52:45 -0500

    Section 10 video 44

--- shitter/ViewModels/TweetActionViewModel.swift
+++ shitter/ViewModels/TweetActionViewModel.swift
@@ -8,6 +8,7 @@ class TweetActionViewModel: ObservableObject {
     
     init(tweet: Tweet){
         self.tweet = tweet
+        checkIfUserLikedTweet()
     }
     
     func likeTweet(){
@@ -17,19 +18,35 @@ class TweetActionViewModel: ObservableObject {
         
         
         COLLECTION_TWEETS.document(tweet.id).updateData(["likes": tweet.likes + 1]) { _ in
-            
             tweetLikesRef.document(uid).setData([:]) { _ in
-                
                 userLikesRef.document(self.tweet.id).setData([:], completion: { _ in
                     self.didLike = true
                 })
             }
         }
-        
     }
     
     func unlikeTweet(){
+        guard let uid = AuthViewModel.shared.userSession?.uid else { return }
+        let tweetLikesRef = COLLECTION_TWEETS.document(tweet.id).collection("tweet-likes")
+        let userLikesRef = COLLECTION_USERS.document(uid).collection("user-likes")
         
+        COLLECTION_TWEETS.document(tweet.id).updateData(["likes": tweet.likes - 1]) { _ in
+            tweetLikesRef.document(uid).delete { _ in
+                userLikesRef.document(self.tweet.id).delete { _ in
+                    self.didLike = false
+                }
+            }
+        }
+    }
+    
+    func checkIfUserLikedTweet(){
+        guard let uid = AuthViewModel.shared.userSession?.uid else { return }
+        let userLikesRef = COLLECTION_USERS.document(uid).collection("user-likes").document(tweet.id)
+        
+        userLikesRef.getDocument { snapshot, _ in
+            guard let didLike = snapshot?.exists else { return }
+            self.didLike = didLike
+        }
     }
 }
-
